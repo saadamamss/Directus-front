5e3ff20966af55b50a8782d16b61881ef57f6a35
Saad Ashraf <saadamams99@gmail.com>
2026-05-29 00:36:37 +0300

    add API proxy rewrites via Vercel for same-origin cookies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.API_PROXY_URL}/api/v1/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
